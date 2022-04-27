3c87ee86f737f828d5862464d52a684bde47cc54
sagarbehal <vivakbehal@gmail.com>
2022-04-27 15:45:13 +0530

    Header google auth added

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom';
-const PageOne =()=>{
-    return <div>PageOne</div>
-}
-const PageTwo =()=>{
-    return <div>PageTwo
-        <button>Click me!</button>
-    </div>
-}
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
 const App = () => {
   return (
-    <div>
-        <BrowserRouter>
-            <div>
-            <Route path='/' exact component={PageOne}/>
-            <Route path='/pagetwo' component={PageTwo}/>
-            </div>
-        </BrowserRouter>
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
