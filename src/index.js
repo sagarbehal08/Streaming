8396d5c8d776e4b2178ce49ba2dc7a70acf659e9
sagarbehal <vivakbehal@gmail.com>
2022-04-28 12:23:16 +0530

    Redux form with validation and error added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
+import reducers from './reducers';
+import {Provider} from 'react-redux'
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <Provider store={store}>
+        <App />
+  </Provider>
+
 );
 
